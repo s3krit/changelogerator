ea1478e750257cf995b31d1daa6e1a458740ba9f
Wilfried Kopp <wilfried@parity.io>
2021-11-09 11:21:03 +0100

    Remove useless tests

--- test/test_meta.rb
+++ test/test_meta.rb
@@ -8,6 +8,7 @@ class TestChangelogerator < Test::Unit::TestCase
   def test_meta_1_commit
     ref1 = '2ebabcec7fcbb3d13a965a852df0559a4aa12a5e'
     ref2 = '9c05f9753b2f939ccf5ba18c08dd4c83c3ab9e0b'
+
     cl = Changelog.new(
       'paritytech/polkadot', ref1, ref2,
       token: ENV['GITHUB_TOKEN'],
@@ -18,14 +19,10 @@ class TestChangelogerator < Test::Unit::TestCase
     assert_equal(1, cl.changes.length)
     assert_equal(%w[A B C], cl.changes[0].meta.keys) # A2 + B0 + C1
 
-    # assert_equal(1, cl.meta.A?.count)
-
     p cl.meta
 
     puts format('JSON Length: %d', j.length)
     assert(j.length > 11_000)
     assert(j.length < 13_000)
-
-    # puts j
   end
 end
